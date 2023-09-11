9f0cecdbcca796361c810e5787c2139e4c4c508f
Ambar <brannstrom9911@gmail.com>
2023-09-11 22:42:02 +0530

    Updated App

--- app/app.js
+++ app/app.js
@@ -34,6 +34,62 @@ app.post('/api/v1/admin/login', (req, res) =>{
     }
 })
 
+app.get('/api/v1/adminslist', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "All Admins."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+app.get('/api/v1/admin/:id', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Single Admin."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+app.put('/api/v1/admin/:id', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Update Admin."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+app.delete('/api/v1/admin/:id', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Delete Admin."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
 app.post('/api/v1/teachers/registration', (req, res) =>{
     try {
         res.status(201).json({
