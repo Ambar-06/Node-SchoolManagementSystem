0814cee92b5eb35f6a2196d5b9c37f67ade38913
Ambar <brannstrom9911@gmail.com>
2023-09-11 22:34:27 +0530

    Updated app

--- app/app.js
+++ app/app.js
@@ -6,4 +6,60 @@ const app = express();
 // =======MIDDLEWARE=========
 app.use(morgan("dev"));
 
+app.post('/api/v1/admin/registration', (req, res) =>{
+    try {
+        res.status(201).json({
+            status: "Success",
+            data : "Admin has been registered."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+app.post('/api/v1/admin/login', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Admin has been LoggedIn."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+app.post('/api/v1/teachers/registration', (req, res) =>{
+    try {
+        res.status(201).json({
+            status: "Success",
+            data : "Teacher has been registered."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+app.post('/api/v1/teachers/login', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Teacher has been LoggedIn."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
 module.exports = app
