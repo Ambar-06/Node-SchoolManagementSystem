b25f4adc7bc6beac07d91f42bcef440c170bb8aa
Ambar <brannstrom9911@gmail.com>
2023-10-02 12:24:01 +0530

    Added some more endpoints

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ const app = express();
 // =======MIDDLEWARE=========
 app.use(morgan("dev"));
 
+// Admin Registration 
 app.post('/api/v1/admin/registration', (req, res) =>{
     try {
         res.status(201).json({
@@ -20,6 +21,7 @@ app.post('/api/v1/admin/registration', (req, res) =>{
     }
 })
 
+// Admin login
 app.post('/api/v1/admin/login', (req, res) =>{
     try {
         res.status(200).json({
@@ -34,6 +36,7 @@ app.post('/api/v1/admin/login', (req, res) =>{
     }
 })
 
+// Admin list
 app.get('/api/v1/adminslist', (req, res) =>{
     try {
         res.status(200).json({
@@ -48,6 +51,7 @@ app.get('/api/v1/adminslist', (req, res) =>{
     }
 })
 
+// Admin Data
 app.get('/api/v1/admin/:id', (req, res) =>{
     try {
         res.status(200).json({
@@ -62,6 +66,7 @@ app.get('/api/v1/admin/:id', (req, res) =>{
     }
 })
 
+//  Update Admin
 app.put('/api/v1/admin/:id', (req, res) =>{
     try {
         res.status(200).json({
@@ -76,6 +81,7 @@ app.put('/api/v1/admin/:id', (req, res) =>{
     }
 })
 
+//  Admin Delete
 app.delete('/api/v1/admin/:id', (req, res) =>{
     try {
         res.status(200).json({
@@ -90,6 +96,96 @@ app.delete('/api/v1/admin/:id', (req, res) =>{
     }
 })
 
+// Teacher suspend by admin
+app.put('/api/v1/admin/suspend/teacher/:id', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Teacher Suspended."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+// Teacher unsuspend by admin
+app.put('/api/v1/admin/unsuspend/teacher/:id', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Teacher Unsuspended."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+// Teacher withdraw by admin
+app.put('/api/v1/admin/withdraw/teacher/:id', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Teacher Withdrawn."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+// Teacher unwithdraw by admin
+app.put('/api/v1/admin/unwithdraw/teacher/:id', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Teacher Unwithdrawn."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+// admin publish exam
+app.put('/api/v1/admin/publish/exam/:id', (req, res) =>{
+    try {
+        res.status(201).json({
+            status: "Success",
+            data : "Exam Published."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+// admin punublish exam
+app.put('/api/v1/admin/unpublish/exam/:id', (req, res) =>{
+    try {
+        res.status(201).json({
+            status: "Success",
+            data : "Exam Unpublished."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+//  Teacher Registration
 app.post('/api/v1/teachers/registration', (req, res) =>{
     try {
         res.status(201).json({
@@ -104,6 +200,7 @@ app.post('/api/v1/teachers/registration', (req, res) =>{
     }
 })
 
+// Teacher login
 app.post('/api/v1/teachers/login', (req, res) =>{
     try {
         res.status(200).json({
@@ -117,5 +214,51 @@ app.post('/api/v1/teachers/login', (req, res) =>{
         })
     }
 })
+
+// Teacher Data
+app.get('/api/v1/teacher/:id', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Teacher Details."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+// Teacher Update
+app.put('/api/v1/teacher/:id', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Teacher Updated."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
+// Teacher Delete
+app.delete('/api/v1/teacher/:id', (req, res) =>{
+    try {
+        res.status(200).json({
+            status: "Success",
+            data : "Teacher Deleted."
+        })
+    } catch (error) {
+        res.json({
+            status: "Failed",
+            data : `Error --> ${error.message}`
+        })
+    }
+})
+
 
 module.exports = app
